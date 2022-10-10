d8ee06239d9b4e082c9250d5283e5521b71c9473
CGessinger <christian@gessinger.de>
2022-10-10 22:21:08 +0200

    Rebuild ProjectDisplayView

--- src/Project/ProjectDisplayView.svelte
+++ src/Project/ProjectDisplayView.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
+    import SvelteMarkdown from 'svelte-markdown'
     import type { ProjectModel } from "./utils/ProjectModel";
     import { terminal } from "$utils/Scripts";
+    import { fs } from "$src/utils/Path";
 
     export let data: ProjectModel;
     let configExists = false;
     data.config_exists().then(exists => configExists = exists);
-
-    function editModel() {
-        // ToDo implement
-        throw new Error("Not implemented");
-    }
-
-    function initModel() {
-        // ToDo implement
-        throw new Error("Not implemented");
-    }
 
     function terminalHere() {
         terminal.terminal_here(data.path);
@@ -25,70 +17,23 @@
     }
 </script>
 
-<div id="project_view">
-    <h1 id="project_name">
+<div class="mt-3 container">
+    <h1 class="text-white">
         {data.name}
-        {#if configExists}
-            <span id="edit_button" on:click="{_ => editModel()}">Edit</span>
-        {:else}
-            <span id="edit_button" on:click="{_ => initModel()}">Init</span>
-        {/if}
     </h1>
-    <div id="scripts_nav">
-        <span class="script_item" on:click="{_ => terminalHere()}">
-            Terminal Here
-        </span>
-        <span class="script_item" on:click="{_ => vscodeHere()}">
-            VS Code Here
-        </span>
+    <ol class="breadcrumb">
+        {#each fs.splitPath(data.path) as part }
+            <li class="breadcrumb-item text-muted">{part}</li>
+        {/each}
+    </ol>
+    <div class="d-flex justify-content-start">
+        <button class="btn btn-dark me-2" on:click="{terminalHere}">Terminal</button>
+        <button class="btn btn-dark" on:click="{vscodeHere}">Editor</button>
     </div>
-    <div id="project_description">
-        {data.description}
+    <div class="text-white mt-3">
+        <SvelteMarkdown source={data.description} />
     </div>
 </div>
 
 <style>
-    #project_view {
-		padding: 0 1rem;
-        overflow-y: scroll;
-    }
-
-    #project_name {
-        font-size: 2rem;
-        font-weight: 600;
-        margin: 0;
-        padding: 0.5rem;
-        text-align: center;
-        color: var(--font-color-dark);
-        background-color: var(--primary-color);
-    }
-
-    #edit_button {
-        float: right;
-        font-size: small;
-        margin: 0.6rem;
-    }
-
-    #scripts_nav {
-        margin: 0;
-        padding: 0;
-        margin-top: 0.8rem;
-    }
-
-    .script_item {
-        display: inline-block;
-        text-align: center;
-        padding: 8px 10px;
-        cursor: pointer;
-        background-color: var(--primary-color);
-        color: var(--font-color-dark);
-        border-radius: 5px;
-        cursor: pointer;
-    }
-
-    #project_description {
-        font-size: 1.2rem;
-        margin: 1rem 1rem 0 1rem;
-        color: var(--font-color-dark);
-    }
 </style>
